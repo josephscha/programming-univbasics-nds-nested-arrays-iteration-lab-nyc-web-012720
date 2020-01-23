59f999dc104d058fb37c3627e5250fe8b41dc505
Joseph S. Cha <josephshincha@gmail.com>
2020-01-23 19:09:29 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -45,4 +45,6 @@ def total_even_pairs(src)
   while row_index < src.count do 
     if src[row_index][0].to_i.even? == true && src[row_index][1].to_i.even? == true 
       new_array << src[row_index][0] + src[row_index][1]
+    end
+    row_index += 1
 end
