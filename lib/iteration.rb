203c9e5180ea44ad1fa4505d3c03a8df06283df8
Joseph S. Cha <josephshincha@gmail.com>
2020-01-23 18:58:09 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,7 +9,10 @@ def join_ingredients(src)
 new_array = []
   row_index = 0 
   while row_index < src.count do 
-    new_array << "I love #{src[row_index][0]} and #{src[row_index[1]}"
+    new_array << "I love #{src[row_index][0]} and #{src[row_index[1]} on my pizza"
+    row_index += 1 
+  end
+  return new_array
 end
 
 def find_greater_pair(src)
